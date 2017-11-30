38e8fe253c03ea3be01599d1d9e3ef845f5cf7ea
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2017-11-30 13:56:20 +0700

    initial commit

---
+++ 02_package/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/gilangmahardhika/golangtraining/02_package/stringutil"
+
+func main() {
+	fmt.Println(stringutil.MyName)
+	fmt.Println(stringutil.Reverse("gnaliG"))
+}
